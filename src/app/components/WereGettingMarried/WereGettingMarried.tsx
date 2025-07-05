06b959170d3510ba626d1117a6a8225d38ab86c7
“CaitlinYoung7” <caitlincnyoung@gmail.com>
2025-07-05 17:24:03 +0100

    Font added, images added slide 2

--- src/app/components/WereGettingMarried/WereGettingMarried.tsx
+++ src/app/components/WereGettingMarried/WereGettingMarried.tsx
@@ -1,9 +1,33 @@
 import styles from "./WereGettingMarried.module.css";
+import Image from "next/image";
 
 export default function WereGettingMarried() {
   return (
     <main className={styles.main}>
-      <h1>WereGettingMarried</h1>
+      <Slider />
+      <Slider />
+      <Slider />
     </main>
   );
 }
+
+const Slider = () => {
+  return (
+    <div className={styles.sliderContainer}>
+      <Phrase />
+      <Phrase />
+      <Phrase />
+    </div>
+  );
+};
+
+const Phrase = () => {
+  return (
+    <div className={styles.singlePhrase}>
+      <h1>We&apos;re Getting Married</h1>
+      <div className={styles.heart}>
+        <Image src="/assets/BlurHeart.png" alt="heart" width={70} height={70} />
+      </div>
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
+"use client";
 import styles from "./page.module.css";
 import Header from "./components/Header/Header";
 import WereGettingMarried from "./components/WereGettingMarried/WereGettingMarried";
 import Footer from "./components/Footer/Footer";
 import AllTheInfo from "./components/AllTheInfo/AllTheInfo";
+import Lenis from "lenis";
+
+import Image from "next/image";
+
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: number) {
+      lenis.raf(time);
+
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <div className={styles.page}>
       <main className={styles.main}>
