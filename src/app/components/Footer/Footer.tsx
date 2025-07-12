ab626c84037199a08baf084abebb1526d86bc812
“CaitlinYoung7” <caitlincnyoung@gmail.com>
2025-07-12 08:59:41 +0100

    "White backgorund added s2"

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,9 +1,16 @@
 import styles from "./Footer.module.css";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <main className={styles.main}>
-      <h1>Footer</h1>
+      <Image
+        src="/assets/SYT.png"
+        alt="heart"
+        width={1300}
+        height={900}
+        style={{ objectFit: "contain" }}
+      />
     </main>
   );
 }
