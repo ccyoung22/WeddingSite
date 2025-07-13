f522dc1e8e72e228810a17c3a7cbfbe773a6964a
“CaitlinYoung7” <caitlincnyoung@gmail.com>
2025-07-13 10:56:37 +0100

    BAckPostCard Added

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,8 +1,18 @@
 import styles from "./Header.module.css";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsFlipped((prev) => !prev);
+    }, 1200);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <main className={styles.main}>
       <motion.div
@@ -15,6 +25,13 @@ export default function Header() {
           className={styles.cassyoung}
           src="/assets/NewPostCard.png"
           alt="Envelope"
+          style={{ display: isFlipped ? "none" : "block" }}
+        />
+        <img
+          className={styles.cassyoung}
+          src="/assets/PostCardBack4.png"
+          alt="Envelope"
+          style={{ display: isFlipped ? "block" : "none" }}
         />
       </motion.div>
 
