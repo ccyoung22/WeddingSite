d1f40272fd98c3ffbd5d4166d3bc338d037812d5
“CaitlinYoung7” <caitlincnyoung@gmail.com>
2025-07-14 18:17:17 +0100

    horizontal scroll woring on page 2

--- src/app/components/AllTheInfo/AllTheInfo.tsx
+++ src/app/components/AllTheInfo/AllTheInfo.tsx
@@ -9,7 +9,11 @@ export default function AllTheInfo() {
   const x = useTransform(scrollYProgress, [0, 0.5], ["-100%", "0%"]);
   const x2 = useTransform(scrollYProgress, [0.5, 1], ["0%", "80%"]);
   const x3 = useTransform(scrollYProgress, [0.5, 1], ["10%", "90%"]);
-  const x4 = useTransform(scrollYProgress, [0.5, 1], ["300%", "600%"]);
+  const x4 = useTransform(scrollYProgress, [0.5, 1], ["10%", "90%"]);
+  const x5 = useTransform(scrollYProgress, [0.6, 1], ["100%", "0%"]);
+
+  // const x5 = useTransform(scrollYProgress, [0, 0.2], ["100%", "400%"]);
+  // const x6 = useTransform(scrollYProgress, [0, 0.5], ["400%", "10%"]);
 
   return (
     <main className={styles.main} ref={targetRef}>
@@ -19,17 +23,27 @@ export default function AllTheInfo() {
             className={styles.contentContainer2}
             style={{ width: x3, height: x2 }}
           >
-            <motion.h1
-              style={{ color: "black", fontSize: x4 }}
-              className={styles.hello}
-            >
-              HELLO
-            </motion.h1>
+            <h1 className={styles.title}>ALL THE INFO</h1>
+            <motion.div
+              className={styles.blurContainer}
+              style={{ opacity: x5 }}
+            ></motion.div>
+            <div className={styles.listDiv}>
+              <motion.ul className={styles.list}>
+                <li>♥ Getting There ♥</li>
+                <li>Where to Stay</li>
+                <li>Schedule</li>
+                <li>RSVP</li>
+                <li>FAQs</li>
+              </motion.ul>
+            </div>
           </motion.div>
         </motion.div>
-        {/* <div className={styles.helloDiv}>
-          <h1>All The Info</h1>
-        </div> */}
+        <div className={styles.helloDiv}>
+          <motion.h1 className={styles.AllTheInfoText}>
+            PACK YOUR BAGS
+          </motion.h1>
+        </div>
       </div>
     </main>
   );
