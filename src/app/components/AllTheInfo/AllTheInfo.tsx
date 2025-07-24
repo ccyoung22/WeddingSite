f54773e1d2e20eaa1ba65c04cc85f633d9c604fa
“CaitlinYoung7” <caitlincnyoung@gmail.com>
2025-07-24 08:53:02 +0100

    Final Page image added

--- src/app/components/AllTheInfo/AllTheInfo.tsx
+++ src/app/components/AllTheInfo/AllTheInfo.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useRouter } from "next/navigation";
 import styles from "./AllTheInfo.module.css";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
@@ -9,8 +11,6 @@ export default function AllTheInfo() {
   const x = useTransform(scrollYProgress, [0, 0.5], ["200%", "0%"]);
   const x2 = useTransform(scrollYProgress, [0.5, 1], ["0%", "100%"]);
   const x3 = useTransform(scrollYProgress, [0.5, 1], ["0%", "100%"]);
-  // const x4 = useTransform(scrollYProgress, [0.5, 1], ["10%", "90%"]);
-  // const opacity = useTransform(scrollYProgress, [0.6, 1], ["50%", "0%"]);
 
   const opacity2 = useTransform(scrollYProgress, [0.6, 1], ["0%", "100%"]);
 
@@ -22,10 +22,12 @@ export default function AllTheInfo() {
       "0 0 0px #4f171e, 0 0 0px #4f171e, 0 0 0px #4f171e, 0 0 0px #4f171e",
     ]
   );
-  // const x6 = useTransform(scrollYProgress, [0.5, 1], ["0%", "150%"]);
 
-  // const x5 = useTransform(scrollYProgress, [0, 0.2], ["100%", "400%"]);
-  // const x6 = useTransform(scrollYProgress, [0, 0.5], ["400%", "10%"]);
+  const router = useRouter();
+
+  function handleClick(route: string) {
+    router.push(route);
+  }
 
   return (
     <main className={styles.main} ref={targetRef}>
@@ -33,7 +35,10 @@ export default function AllTheInfo() {
         <motion.div className={styles.contentContainer} style={{ x }}>
           <motion.div
             className={styles.contentContainer2}
-            style={{ width: x3, height: x2 }}
+            style={{
+              width: x3,
+              height: x2,
+            }}
           >
             <div className={styles.listImgDiv}>
               <div className={styles.folderDiv}>
@@ -49,11 +54,36 @@ export default function AllTheInfo() {
                 style={{ textShadow, opacity: opacity2 }}
               >
                 <ul className={styles.list}>
-                  <li>Getting There</li>
-                  <li>Where to Stay</li>
-                  <li>Schedule</li>
-                  <li>RSVP</li>
-                  <li>FAQs</li>
+                  <li
+                    onClick={() => handleClick("/getting-there")}
+                    className={styles.listItem}
+                  >
+                    Getting There
+                  </li>
+                  <li
+                    onClick={() => handleClick("/where-to-stay")}
+                    className={styles.listItem}
+                  >
+                    Where to Stay
+                  </li>
+                  <li
+                    onClick={() => handleClick("/schedule")}
+                    className={styles.listItem}
+                  >
+                    Schedule
+                  </li>
+                  <li
+                    onClick={() => handleClick("/rsvp")}
+                    className={styles.listItem}
+                  >
+                    RSVP
+                  </li>
+                  <li
+                    onClick={() => handleClick("/faqs")}
+                    className={styles.listItem}
+                  >
+                    FAQs
+                  </li>
                 </ul>
               </motion.div>
             </div>
