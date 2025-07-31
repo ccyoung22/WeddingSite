9d02e5718d7c58e2217a53786ee1d2c2f958bd12
“CaitlinYoung7” <161062056+ccyoung22@users.noreply.github.com>
2025-07-31 17:05:08 +0100

    Schedule page finshed

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -18,62 +18,36 @@ export default function Page() {
         />
 
         <div className={styles.contentDiv}>
-          <div className={styles.container}>
-            <div className={styles.scheduleItem}>
-              <div className={styles.timeline}>
-                <div className={styles.timeMarker}>
-                  <div className={styles.dot}></div>
-                  <div className={styles.line}></div>
-                </div>
-                <div className={styles.timeLabel}>
-                  Friday
-                  <br />
-                  19th June
-                </div>
-              </div>
-              <div className={styles.eventDetails}>
-                <h3 className={styles.eventTitle}>Casual Evening Drinks</h3>
-                <p className={styles.location}>*Location to be confirmed</p>
-              </div>
-            </div>
+          <div className={styles.lineDotDiv}>
+            <div className={styles.dot}></div>
+            <div className={styles.dot2}></div>
+            <div className={styles.dot3}></div>
 
-            <div className={styles.scheduleItem}>
-              <div className={styles.timeline}>
-                <div className={styles.timeMarker}>
-                  <div className={styles.dot}></div>
-                  <div className={styles.line}></div>
-                </div>
-                <div className={styles.timeLabel}>
-                  Saturday
-                  <br />
-                  20th June
-                </div>
-              </div>
-              <div className={styles.eventDetails}>
-                <h3 className={styles.eventTitle}>The Big Day</h3>
-                <p className={styles.eventType}>
-                  Wedding ceremony, Dinner and Drinks
-                </p>
-                <p className={styles.time}>4pm(ish) till late</p>
-              </div>
-            </div>
+            <div className={styles.line}></div>
+          </div>
 
-            <div className={styles.scheduleItem}>
-              <div className={styles.timeline}>
-                <div className={styles.timeMarker}>
-                  <div className={styles.dot}></div>
-                </div>
-                <div className={styles.timeLabel}>
-                  Sunday
-                  <br />
-                  21st June
-                </div>
-              </div>
-              <div className={styles.eventDetails}>
-                <h3 className={styles.eventTitle}>Pool Party</h3>
-                <p className={styles.eventType}>Pizza and beers by the pool</p>
-                <p className={styles.time}>Midday onwards</p>
-              </div>
+          <div className={styles.scheduleInfomration}>
+            <div className={styles.dayGroup}>
+              <div className={styles.timeLabel}>Friday 19th June</div>
+
+              <h3 className={styles.eventTitle}>Casual Evening Drinks</h3>
+              <p className={styles.location}>Location to be confirmed</p>
+            </div>
+            <div className={styles.dayGroup}>
+              <div className={styles.timeLabel}>Saturday 20th June</div>
+
+              <h3 className={styles.eventTitle}>Wedding Day</h3>
+              <p className={styles.eventType}>
+                Wedding ceremony, Dinner and Drinks
+              </p>
+              <p className={styles.time}>4pm(ish) till late</p>
+            </div>
+            <div className={styles.dayGroup}>
+              <div className={styles.timeLabel}>Sunday 21st June</div>
+
+              <h3 className={styles.eventTitle}>Pool Party</h3>
+              <p className={styles.eventType}>Pizza and beers by the pool</p>
+              <p className={styles.time}>Midday onwards</p>
             </div>
           </div>
         </div>
