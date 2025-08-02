e08a75784f309e86151af6125d857a66c78c820a
“CaitlinYoung7” <161062056+ccyoung22@users.noreply.github.com>
2025-08-02 08:27:40 +0100

    Dynamic View Height added

--- src/app/rsvp/page.tsx
+++ src/app/rsvp/page.tsx
@@ -1,5 +1,9 @@
 import styles from "./page.module.css";
 
 export default function Page() {
-  return <div className={styles.main}>RSVPs</div>;
+  return (
+    <div className={styles.main}>
+      <img src={"/assets/RSVP.png"} className={styles.paperImg}></img>
+    </div>
+  );
 }
