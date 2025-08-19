47aaaccc4c9bb08f999ecc099ee0b04de4458bed
“CaitlinYoung7” <161062056+ccyoung22@users.noreply.github.com>
2025-08-19 08:03:07 +0100

    new rsvp added

--- src/app/rsvp/page.tsx
+++ src/app/rsvp/page.tsx
@@ -17,35 +17,7 @@ export default function Page() {
   return (
     <div className={styles.main}>
       <div className={styles.imageContainer}>
-        <img src={"/assets/RSVP2.png"} className={styles.paperImg} />
-        <div className={styles.textOverlay}>
-          <p>When you RSVP, please include:</p>
-          <ul className={styles.list}>
-            <li>- Names of all guests attending</li>
-            <li>- Any dietary requirements or allergies</li>
-            <li>
-              - Which events {`you'll`} be attending (Friday drinks, Saturday
-              wedding, Sunday pool party).
-            </li>
-          </ul>
-          <p className={styles.rsvpText}>
-            <strong>
-              Please send all RSVP information by <strong>November 1st</strong>{" "}
-              directly to Caitlin or Jake
-            </strong>
-          </p>
-          <p>
-            Please let us know where {`you'll `}
-            be staying by 1st Feburary 2026 at the latest. This will help us
-            organise transport and ensures everything runs smoothly over the
-            weekend.{" "}
-          </p>
-          <p>
-            {" "}
-            If you have any questions, please feel free to message either of us
-            anytime, {`we're`} happy to help with anything you need to know.
-          </p>
-        </div>
+        <img src={"/assets/RSVPtext.webp"} className={styles.paperImg} />
       </div>
     </div>
   );
